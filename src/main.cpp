5ae041654fad209a94212bbdaba55c3a774bda93
JustinNarayan <38253333+JustinNarayan@users.noreply.github.com>
2024-04-10 16:57:29 -0400

    Testing updates

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <l298n.h>
 #include <drivetrain.h>
 #include <controller.h>
+#include <lcd1602.h>
 
 #define ALERT_TIME 500
 
@@ -12,15 +13,14 @@ static Controller controller;
 static L298N leftMotor;
 static L298N rightMotor;
 static drivetrain driver;
+static LCD1602 display;
 
 static void blinkTask(void *parameters)
 {
   while (1)
   { 
-    SERIAL_PORT.println("HIGH");
     digitalWrite(LED_PIN, HIGH);
     vTaskDelay(LED_TIME / portTICK_PERIOD_MS);
-    SERIAL_PORT.println("LOW");
     digitalWrite(LED_PIN, LOW);
     vTaskDelay(LED_TIME / portTICK_PERIOD_MS);
   }
@@ -41,23 +41,78 @@ static void pollControllerTask(void* parameters) {
   }
 }
 
+static void outputControllerTask(void* parameters) {
+  while(1) {
+    if(controller.direction == DIRECTION_FORWARD)
+      SERIAL_PORT.print("Forward\t");
+    else if(controller.direction == DIRECTION_BACKWARD)
+      SERIAL_PORT.print("Backward\t");
+    else
+      SERIAL_PORT.print("None\t");
+
+    if(controller.turn == TURN_LEFT)
+      SERIAL_PORT.print("Left\t");
+    else if(controller.turn == TURN_RIGHT)
+      SERIAL_PORT.print("Right\t");
+    else
+      SERIAL_PORT.print("None\t");
+
+    SERIAL_PORT.println();
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+static void motorTestTask(void* parameters) {
+  while(1) {
+    SERIAL_PORT.println("Forward!");
+    L298N_forward(&leftMotor, 0x10);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+    SERIAL_PORT.println("Halt!");
+    L298N_halt(&leftMotor);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+
+    SERIAL_PORT.println("Backward!");
+    L298N_forward(&leftMotor, 0x10);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+    SERIAL_PORT.println("Halt!");
+    L298N_halt(&leftMotor);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
 
 static void initTask(void *parameters)
 {
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+
   // Enable LED
   pinMode(LED_PIN, OUTPUT);
 
   // Instantiate drivetrain
-  drivetrain_init(&driver, &leftMotor, &rightMotor);
+  // drivetrain_init(&driver, &leftMotor, &rightMotor);
 
   // Instantiate controller
   controller_init(&controller);
+
+  // Instantiate LCD
+  // lcd1602_init(&display);
 
   // Create blink task
   generateTask(blinkTask, "Blink", NULL);
 
   // Create controller poll task
   generateTask(pollControllerTask, "Controller poll", NULL);
+  
+  // Generate controller output task
+  generateTask(outputControllerTask, "Controller output", NULL);
+
+  // Generate motor test task
+  // L298N_init(&leftMotor, PD0, PD1, PG0);
+  // generateTask(motorTestTask, "Motor test", NULL);
 
   SERIAL_PORT.println("Completed init");
   vTaskDelete(NULL);
